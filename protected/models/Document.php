70ec56304f41059ec51427d2c9d50bd13fb2d2d7
andrew <shavneval@oblgas.by>
2016-08-04 11:29:36 +0300

    округление сумм по документу до суммирования

--- protected/models/Document.php
+++ protected/models/Document.php
@@ -78,12 +78,12 @@ class Document extends CActiveRecord
 			'doctype'       => array(self::BELONGS_TO, 'Doctype', 'id_doctype'),
 			'docaddition'   => array(self::BELONGS_TO, 'Docaddition', 'id'),
 
-			'sum_cost'      => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(quantity*cost)'),
-			'sum_markup'    => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM((quantity*cost*(markup/100)))'),
-			'sum_vat'       => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM((vat/100)*(quantity*cost*(1+markup/100)))'),
-			'sum_vat2'      => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM((vat/100)*(quantity*price))'),
+			'sum_cost'      => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST(quantity*cost as numeric),2))'),
+			'sum_markup'    => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST((quantity*cost*(markup/100)) as numeric),2))'),
+			'sum_vat'       => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST((vat/100)*(quantity*cost*(1+markup/100)) as numeric),2))'),
+			'sum_vat2'      => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST((vat/100)*(quantity*price) as numeric),2))'),
 			'sum_price'     => array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST(quantity*price as numeric),2))'),
-			'sum_with_markup'=>array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM((markup/100+1)*cost*quantity)'),
+			'sum_with_markup'=>array(self::STAT,  'Documentdata', 'id_doc', 'select' => 'SUM(round(CAST((markup/100+1)*cost*quantity as numeric),2))'),
 		);
 	}
 
