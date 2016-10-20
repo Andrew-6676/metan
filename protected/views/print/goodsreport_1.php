3d0ce7f0410df7ecf3a14e8556e6916049c2680d
andrew <shavneval@oblgas.by>
2016-10-20 11:27:02 +0300

    добавил к остатку сумму по расрочке

--- protected/views/print/goodsreport_1.php
+++ protected/views/print/goodsreport_1.php
@@ -731,7 +731,7 @@ kred:
 			<table class="total no_border" >
 				<tr class="itog">
 					<td class="itog">Остаток на <?php echo Utils::format_date($_GET['to_date']); ?></td>
-					<td class="itog r"><?php echo number_format($total, '2','.',' '); ?></td>
+					<td class="itog r"><?php echo number_format($total+$itogo_r2, '2','.',' '); ?></td>
 				</tr>
 			</table>
 		</div>
